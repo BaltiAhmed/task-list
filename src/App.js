90b1936bbfeebfba90addf661b53a60544311663
GMC2020 <ahmedbalti06@gmail.com>
2020-02-21 21:59:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+      textdecoration: "none",
+      items: []
+    };
+  }
+
+  onChange(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    this.setState({
+      userInput: '',
+      items: [...this.state.items, this.state.userInput]
+    });
+  }
+
+
+  deleteTodo(item) {
+
+    
+    
+    this.setState({
+      textdecoration: "line-through"
+    });
+  }
+
+  renderTodos() {
+
+    return this.state.items.map((item) => {
+      return (
+        <div key={item}>
+          <h3
+            onClick={this.deleteTodo.bind(this, item)}
+            style={{ textDecoration: this.state.textdecoration }}>{item}
+          </h3>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 >Ma Todo list</h1>
+        <form >
+          <input
+            value={this.state.userInput}
+            type="text"
+            placeholder="Renseigner un item"
+            onChange={this.onChange.bind(this)}
+
+          />
+          <button
+            onClick={this.addTodo.bind(this)}
+
+          >
+            Ajouter
+                </button>
+        </form>
+        <div>
+          {this.renderTodos()}
+        </div>
+      </div>
+    );
+  }
+}
